3c258b80f40317db1d39bb92dab21f3e91015c0e
mazharulislam-12 <505mazharulislam@gmail.com>
2023-08-27 08:24:11 -0700

    show all button

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,5 @@
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     const data = await res.json()
     const phones = data.data
     // console.log(phones)
@@ -10,12 +10,27 @@ const displayPhones = phones => {
     // console.log(phones);
     //step 1
     const phoneContainer = document.getElementById('phone-container')
+    //clear phone container card before adding new card
+    phoneContainer.textContent = '';
+
+    //display show all button if there are more then 12 phone
+    const showAllContainer = document.getElementById('show-all-container')
+    if (phones.length > 12) {
+        showAllContainer.classList.remove('hidden')
+    }
+    else{
+        showAllContainer.classList.add('hidden')
+    }
+
+
+    //display only first 12 phone
+    phones = phones.slice(0,12);
 
     phones.forEach(phone => {
         console.log(phone);
         //2. create a div
         const phoneCard = document.createElement('div');
-        phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`;
+        phoneCard.classList = `card bg-gray-100 p-4 shadow-xl`;
         // 3. set inner Html
         phoneCard.innerHTML = `
 
@@ -36,4 +51,12 @@ const displayPhones = phones => {
 }
 
 
-loadPhone()
+//handle search button
+const handleSearch = () =>{
+    const searchField = document.getElementById('search-field')
+    const searchText = searchField.value;
+    console.log(searchText)
+    loadPhone(searchText)
+}
+
+// loadPhone()
